a60f1cf10d8a7fecda7c2c2351944d99172a3fcc
ktr0731 <ktr@syfm.me>
2018-12-21 20:01:20 +0900

    real 0.1.0

--- go.mod
+++ go.mod
@@ -1,1 +1,11 @@
 module github.com/ktr0731/go-updater-test
+
+require (
+	github.com/blang/semver v3.5.1+incompatible
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/inconshreveable/go-update v0.0.0-20160112193335-8152e7eb6ccf // indirect
+	github.com/ktr0731/go-semver v0.1.1
+	github.com/ktr0731/go-updater v0.1.3
+	golang.org/x/arch v0.0.0-20181203225421-5a4828bb7045 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	semver "github.com/ktr0731/go-semver"
+	updater "github.com/ktr0731/go-updater"
+	"github.com/ktr0731/go-updater/github"
+)
+
+var version = semver.MustParse("0.1.0")
+
+var (
+	v        = flag.Bool("v", false, "show version")
+	doUpdate = flag.Bool("update", false, "do update")
+)
 
 func main() {
-	fmt.Println("v0.1.0")
+	flag.Parse()
+
+	if *v {
+		fmt.Println(version)
+		return
+	}
+
+	means, err := updater.NewMeans(github.GitHubReleaseMeans("ktr0731", "go-updater-test", github.TarGZIPDecompresser))
+	if err != nil {
+		log.Fatal(err)
+	}
+	u := updater.New(version, means)
+	u.UpdateIf = updater.FoundPatchUpdate
+
+	fmt.Printf("current: %s\n", version)
+	fmt.Println("checking updates...")
+	updatable, latest, err := u.Updatable(context.Background())
+	if updatable {
+		fmt.Printf("updatable: %s to %s\n", version, latest)
+	} else {
+		fmt.Println("not found")
+	}
+
+	if *doUpdate {
+		fmt.Println("updating...")
+	}
 }
